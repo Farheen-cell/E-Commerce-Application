85bc3a2adc883cb91818b7fe763985117c8ef1ed
Farheen <103572478+Farheen-cell@users.noreply.github.com>
2023-11-09 12:36:58 +0530

    Update SwaggerConfig.java
    update swagger info

--- ECommerceApplication/ECommerceApplication/src/main/java/com/app/config/SwaggerConfig.java
+++ ECommerceApplication/ECommerceApplication/src/main/java/com/app/config/SwaggerConfig.java
@@ -1,12 +1,13 @@
 package com.app.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,7 +17,7 @@ public class SwaggerConfig {
 		return new OpenAPI().info(new Info().title("E-Commerce Application")
 			.description("Backend APIs for E-Commerce app")
 			.version("v1.0.0")
-			.contact(new Contact().name("Shaik Sirajuddin").url("https://sirajuddin135.github.io/").email("shaiksirajuddin135@gmail.com"))
+			.contact(new Contact().name("Farheen Ali").url("https://github.com/Farheen-cell").email("farheen.ali108@gmail.com"))
 			.license(new License().name("License").url("/")))
 			.externalDocs(new ExternalDocumentation().description("E-Commerce App Documentation")
 			.url("http://localhost:8080/swagger-ui/index.html"));
